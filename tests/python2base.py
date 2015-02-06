c1099a5fd4a872dfd94935f2b9e2a76b50998ce1
timmartin19 <tim.martin@vertical-knowledge.com>
2015-02-06 16:02:13 -0500

    Fixed dictionary comparison

--- tests/python2base.py
+++ tests/python2base.py
@@ -4,11 +4,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from unittest import TestCase
 
-
-import difflib
-import pprint
-
-from unittest.util import safe_repr
 __author__ = 'Tim Martin'
 
 
@@ -22,11 +17,4 @@ class TestBase(TestCase):
     def assertDictEqual(self, d1, d2, msg=None):
         self.assertIsInstance(d1, dict, 'First argument is not a dictionary')
         self.assertIsInstance(d2, dict, 'Second argument is not a dictionary')
-
-        if d1 != d2:
-            standardMsg = '%s != %s' % (safe_repr(d1, True), safe_repr(d2, True))
-            diff = ('\n' + '\n'.join(difflib.ndiff(
-                           pprint.pformat(d1).splitlines(),
-                           pprint.pformat(d2).splitlines())))
-            standardMsg = self._truncateMessage(standardMsg, diff)
-            self.fail(self._formatMessage(msg, standardMsg))
+        self.assertTrue(d1 == d2)
