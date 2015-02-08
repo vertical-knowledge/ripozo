bd6ac8e86e65b268b952eda8679fcb2329f56df7
timmartin19 <tim.martin@vertical-knowledge.com>
2015-02-07 23:03:14 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ __author__ = 'Tim Martin'
 from setuptools import setup, find_packages
 from os import path
 
-version = '0.1.0'
+version = '0.1.2b2'
 
 setup(
     name='ripozo',
     version=version,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
-    description='An tool for easily making cassandra RESTful interfaces',
+    description='An tool for easily making RESTful interfaces',
     author='Tim Martin',
     author_email='tim.martin@vertical-knowledge.com',
     install_requires=['six'],
@@ -19,7 +19,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     test_suite="tests"
