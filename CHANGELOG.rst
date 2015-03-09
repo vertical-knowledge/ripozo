e71989bf12a7a3996fcd124addf7fa27d80941e3
timmartin19 <tim.martin@vertical-knowledge.com>
2015-03-08 23:24:34 -0400

    Preparing release 0.1.11

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,4 @@
-0.1.11 (unreleased)
+0.1.11 (2015-03-08)
 ===================
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 import os
 
-version = '0.1.11.dev0'
+version = '0.1.11'
 
 base_dir = os.path.dirname(__file__)
 
