8723bddc9907f7731535033a39914078d2cbef88
timmartin19 <tim.martin@vertical-knowledge.com>
2015-03-24 21:53:45 -0400

    Added test for issue #10
    Fixes #10

--- tests/integration/resources.py
+++ tests/integration/resources.py
@@ -29,19 +29,54 @@ class TestResourceIntegration(TestBase, unittest.TestCase):
             def hello(cls, request, *args, **kwargs):
                 return cls(properties=request.body_args)
 
+        self.help_test_validate_with_manager_field_validators(TestValidateIntegrations)
+
+    def test_validate_with_manager_field_validators_inherited(self):
+        """
+        Same as test_validate_with_manager_field_validators,
+        except that it checks it on an inherited class.  This was
+        done to ensure no regressions in regards to issue #10 on github.
+        """
+        fake_manager = mock.MagicMock()
+        fake_manager.field_validators = [BaseField('first', required=True), BaseField('second', required=False)]
+
+        class TestValidateIntegrationsParent(ResourceBase):
+            manager = fake_manager
+
+            @apimethod(methods=['GET'])
+            @translate(manager_field_validators=True, validate=True)
+            def hello(cls, request, *args, **kwargs):
+                return cls(properties=request.body_args)
+
+        self.help_test_validate_with_manager_field_validators(TestValidateIntegrationsParent)
+
+        class TestValidateIntegrationsInherited(TestValidateIntegrationsParent):
+            _resource_name = 'another'
+            pass
+
+        self.help_test_validate_with_manager_field_validators(TestValidateIntegrationsInherited)
+
+    def help_test_validate_with_manager_field_validators(self, klass):
+        """
+        Helper for the couple test_validate_with_manager_field_validators.
+        Just checks that it gets the right responses.  Abstracted out poorly
+        to reduce code duplication and because I hate typing.
+
+        :param type klass: The class that you are checking.
+        """
         request = RequestContainer(body_args=dict(first=[1], second=[2]))
-        response = TestValidateIntegrations.hello(request)
+        response = klass.hello(request)
         self.assertDictEqual(dict(first=1, second=2), response.properties)
         self.assertDictEqual(request.body_args, response.properties)
 
         # Without list inputs since requests are mutable
-        response = TestValidateIntegrations.hello(request)
+        response = klass.hello(request)
         self.assertDictEqual(dict(first=1, second=2), response.properties)
         self.assertDictEqual(request.body_args, response.properties)
 
         request2 = RequestContainer(body_args=dict(second=[2]))
-        self.assertRaises(ValidationException, TestValidateIntegrations.hello, request2)
+        self.assertRaises(ValidationException, klass.hello, request2)
 
         request3 = RequestContainer(body_args=dict(first=[1]))
-        response = TestValidateIntegrations.hello(request3)
+        response = klass.hello(request3)
         self.assertDictEqual(dict(first=1), response.properties)
