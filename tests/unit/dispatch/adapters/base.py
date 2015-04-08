16fc2855ece8a5dc8b6fd7e1a3e5ad66969980d1
timmartin19 <tim.martin@vertical-knowledge.com>
2015-04-07 22:29:09 -0400

    Added helpers for testing adapters

--- tests/unit/dispatch/adapters/base.py
+++ tests/unit/dispatch/adapters/base.py
@@ -7,6 +7,9 @@ import six
 import unittest
 
 from ripozo.dispatch.adapters.base import AdapterBase
+from ripozo.viewsets.relationships import Relationship, ListRelationship
+from ripozo.viewsets.resource_base import ResourceBase
+
 from ripozo_tests.python2base import TestBase
 
 __author__ = 'Tim Martin'
@@ -23,4 +26,40 @@ class TestAdapter(AdapterBase):
 
 
 class TestAdapterBase(TestBase, unittest.TestCase):
-    pass
+    def get_related_resource_class(self):
+        class Related(ResourceBase):
+            _resource_name = 'related'
+            _pks = ['id']
+        return Related
+
+    def get_relationship_resource_class(self):
+        class RelationshipResource(ResourceBase):
+            _resource_name = 'relationship_resource'
+            _relationships = {
+                'relationship': Relationship(property_map=dict(id='id'), relation='Related')
+            }
+        return RelationshipResource
+
+    def get_list_relationship_resource_class(self):
+        class RelationshipResource(ResourceBase):
+            _resource_name = 'relationship_resource'
+            _relationships = {
+                'relationship': ListRelationship('list_relationship', relation='Related')
+            }
+        return RelationshipResource
+
+    def get_link_resource_class(self):
+        class LinkResource(ResourceBase):
+            _resource_name = 'link_resource'
+            _links = {
+                'link': Relationship('linked', relation='Related')
+            }
+        return LinkResource
+
+    def get_link_list_resource_class(self):
+        class LinkListResource(ResourceBase):
+            _resource_name = 'link_list_resource'
+            _links = {
+                'link': Relationship('linked_list', relation='Related')
+            }
+        return LinkListResource
