446b3dad900212db14d5918549b663249c4fc938
timmartin19 <tim.martin@vertical-knowledge.com>
2015-04-15 23:25:00 -0400

    Flattened imports...

--- ripozo/__init__.py
+++ ripozo/__init__.py
@@ -3,5 +3,9 @@ import pkg_resources
 __version__ = pkg_resources.get_distribution('ripozo').version
 
 
+from ripozo.decorators import apimethod, translate
+from ripozo.dispatch import adapters
+from ripozo.viewsets import fields
+from ripozo.viewsets.relationships.list_relationship import ListRelationship
+from ripozo.viewsets.relationships.relationship import Relationship
 from ripozo.viewsets.resource_base import ResourceBase
-from ripozo.decorators import apimethod
--- ripozo/viewsets/fields/__init__.py
+++ ripozo/viewsets/fields/__init__.py
@@ -3,5 +3,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from ripozo.viewsets.fields.base import BaseField
 from ripozo.viewsets.fields.common import StringField, IntegerField, \
-    BooleanField, BaseField, DateTimeField, FloatField
+    BooleanField, BaseField, DateTimeField, FloatField, ListField
