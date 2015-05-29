2af44306d4f790534205b419951c5269771885d7
timmartin19 <tim.martin@vertical-knowledge.com>
2015-05-29 18:33:38 -0400

    Goddammit.

--- ripozo/viewsets/fields/__init__.py
+++ ripozo/viewsets/fields/__init__.py
@@ -5,4 +5,4 @@ from __future__ import unicode_literals
 
 from ripozo.viewsets.fields.base import BaseField
 from ripozo.viewsets.fields.common import StringField, IntegerField, \
-    BooleanField, BaseField, DateTimeField, FloatField, ListField
+    BooleanField, BaseField, DateTimeField, FloatField, ListField, DictField
