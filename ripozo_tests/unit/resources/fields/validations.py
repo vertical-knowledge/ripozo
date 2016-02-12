d89caa8703c9dcee5ecf68c6b2bffbb414420e5a
tmartin <tim.martin@vertical-knowledge.com>
2016-02-11 23:51:41 -0500

    Partial implementation of tests for validation helpers

--- ripozo_tests/unit/resources/fields/validations.py
+++ ripozo_tests/unit/resources/fields/validations.py
@@ -5,6 +5,8 @@ from __future__ import unicode_literals
 
 import unittest2
 
+from ripozo.exceptions import ValidationException
+from ripozo.resources.fields.field import Field
 from ripozo.resources.fields.validations import translate_iterable_to_single, \
     validate_required, validate_type, validate_size, validate_regex, \
     basic_validation
@@ -15,38 +17,59 @@ class TestValidation(unittest2.TestCase):
         """
         Tests the case where the item is an iterable
         """
-        assert False
+        resp = translate_iterable_to_single([1, 2])
+        self.assertEqual(resp, 1)
 
     def test_translate_iterable_to_single_not_list(self):
         """
         Tests the case where the item is not an iterable
         """
-        assert False
+        resp = translate_iterable_to_single(1)
+        self.assertEqual(resp, 1)
 
     def test_validate_required_success(self):
         """
         Tests when the object is required and it is there
         """
-        assert False
+        f = Field('f', required=True)
+        resp = validate_required(f, 1)
+        self.assertEqual(resp, 1)
 
     def test_validate_required_skipped(self):
         """
         Tests when the item is None and the validation is skipped
         because skip_required is True
         """
-        assert False
+        f = Field('f', required=True)
+        resp = validate_required(f, None, skip_required=True)
+        self.assertEqual(resp, None)
+
+    def test_validate_required_non_required_field(self):
+        """
+        Tests the the field is validated if the field
+        is not required and the object is None
+        """
+        f = Field('f', required=False)
+        resp = validate_required(f, None, skip_required=False)
+        self.assertEqual(resp, None)
 
     def test_validate_required_failure(self):
         """Tests that it fails when the object is None"""
-        assert False
+        f = Field('f', required=True)
+        self.assertRaises(ValidationException, validate_required, f, None)
 
     def test_validate_type(self):
         """Success case for validate_type"""
-        assert False
+        f = Field('f')
+        obj = object()
+        resp = validate_type(f, object, obj)
+        self.assertIs(resp, obj)
 
     def test_validate_type_failure(self):
         """Failure case for validate_type"""
-        assert False
+        f = Field('f')
+        obj = 'blah'
+        self.assertRaises(ValidationException, validate_type, f, int, obj)
 
     def test_validate_size(self):
         """Success case for validate size"""
